1e8b98318a0d16ddca0926ef89a4ea39b3b87e83
Liaisan Akhmetova <akhmetova1423@gmail.com>
2022-03-23 16:53:16 +0300

    new checkbox filter with bugs

--- src/components/ui/CheckboxNew/CheckboxNew.tsx
+++ src/components/ui/CheckboxNew/CheckboxNew.tsx
@@ -2,19 +2,22 @@ import styles from "./CheckboxNew.module.scss";
 import { FC, useState } from "react";
 
 interface CheckboxNew {
+  id:number,
   text: string,
+  handleClick?: Function,
+  isChecked: boolean
 }
 
-export const CheckboxNew: FC<CheckboxNew> = ({ text }) => {
+export const CheckboxNew: FC<CheckboxNew> = ({ id, text , handleClick, isChecked}) => {
 
-  const [checked, setChecked] = useState(false);
+  // const [checked, setChecked] = useState(false);
 
   return (
     <>
       <label className={styles.checkbox}>
-        <input type="checkbox" className={styles.checkbox__input} onClick={() => setChecked(!checked)} />
-        <span className={checked ? styles.checkbox__activeSpan : styles.checkbox__span}/>
-        <p className={checked ? styles.checkbox__activeText : styles.checkbox__text}>{text}</p>
+        <input id={`${id}`} type="checkbox" className={styles.checkbox__input} onChange={()=>handleClick} checked={isChecked}/>
+        <span className={isChecked ? styles.checkbox__activeSpan : styles.checkbox__span} onClick={()=>handleClick}/>
+        <p className={isChecked ? styles.checkbox__activeText : styles.checkbox__text}>{text}</p>
       </label>
     </>
   );
--- src/pages/Ecomarket/Ecomarket.tsx
+++ src/pages/Ecomarket/Ecomarket.tsx
@@ -1,169 +1,207 @@
-import { observer } from "mobx-react";
+import { useEffect, useState } from "react";
 import { useStores } from "../../utils/use-stores-hook";
-import {BaseLayout} from "../../components/BaseLayout/BaseLayout";
-import {EcoMarketCard} from "../../components/EcoMarketCard/EcoMarketCard";
-import {Button} from "../../components/ui/Button/Button";
+import { BaseLayout } from "../../components/BaseLayout/BaseLayout";
+import { EcoMarketCard } from "../../components/EcoMarketCard/EcoMarketCard";
+import { Button } from "../../components/ui/Button/Button";
 import { CheckboxNew } from "../../components/ui/CheckboxNew/CheckboxNew";
-import {QrModal} from "../../components/Modals/QrModal/QrModal";
-import sneak1 from '../../svg-icons/ecoMarket/sneak1.svg';
-import sneak2 from '../../svg-icons/ecoMarket/sneak2.svg';
-import sneak3 from '../../svg-icons/ecoMarket/sneak3.svg';
-import sneak4 from '../../svg-icons/ecoMarket/sneak4.svg';
-import hoodie from '../../svg-icons/ecoMarket/hoodie.svg';
-import currency from '../../svg-icons/currency.svg';
-import styles from './Ecomarket.module.scss';
+import { QrModal } from "../../components/Modals/QrModal/QrModal";
+import { Genders, GoodsType, Brands, CheckboxModel } from "../../stores/CheckboxStore";
+import sneak1 from "../../svg-icons/ecoMarket/sneak1.svg";
+import sneak2 from "../../svg-icons/ecoMarket/sneak2.svg";
+import sneak3 from "../../svg-icons/ecoMarket/sneak3.svg";
+import sneak4 from "../../svg-icons/ecoMarket/sneak4.svg";
+import hoodie from "../../svg-icons/ecoMarket/hoodie.svg";
+import currency from "../../svg-icons/currency.svg";
+import styles from "./Ecomarket.module.scss";
 
 const onGetPromocodeClick = () => {
-    console.log("click");
-}
+  console.log("click");
+};
 
 const onButtonClick = () => {
   console.log("click");
-}
+};
 
-export const Ecomarket = observer(() => {
+export const Ecomarket = () => {
 
-    const { modalStore: {setCurrentModal } } = useStores();
+  const { modalStore: { setCurrentModal } } = useStores();
+  const [isCheck, setIsCheck] = useState<number[]>([]);
+  const [isCheckedAll, setIsCheckedAll] = useState(false);
+  const [genders, setGenders] = useState<Array<CheckboxModel>>([]);
+  const [goodsType, setGoodsType] = useState<Array<CheckboxModel>>([]);
+  const [brands, setBrands] = useState<Array<CheckboxModel>>([]);
 
-    const onGetQRGet = () => {
-        setCurrentModal(QrModal);
+  useEffect(() => {
+    setGenders(Genders);
+    setGoodsType(GoodsType);
+    setBrands(Brands);
+  }, [genders, goodsType, brands]);
+
+  const onGetQRGet = () => {
+    setCurrentModal(QrModal);
+  };
+
+  const handleSelectAll = () => {
+    console.log("dd")
+    setIsCheckedAll(!isCheckedAll);
+    setIsCheck(goodsType.map(li => li.id));
+    if (isCheckedAll) {
+      setIsCheck([]);
     }
-    const {checkboxStore: {genders, goodsType, brands}} = useStores();
+  };
 
-    return (
-        <>
-            <BaseLayout>
-                <div className={styles.ecomarket}>
-                    <h1>ЭкоМаркет</h1>
-                    <div className={styles.ecomarket__filterButtons}>
-                        <Button
-                          type={"button"}
-                          theme={"eco"}
-                          onClick={onButtonClick}
-                          children={"По популярности"}
-                        />
+  const handleSelect = (e:any) => {
+    const {id, isChecked} = e.target;
+    console.log("s");
+    setIsCheck([...isCheck, id]);
+    if(!isChecked){
+      setIsCheck(isCheck.filter(item => item !== id))
+    }
+  }
 
-                        <Button
-                          type={"button"}
-                          theme={"eco"}
-                          onClick={onButtonClick}
-                          children={"По цене"}
-                        />
+  return (
+    <>
+      <BaseLayout>
+        <div className={styles.ecomarket}>
+          <h1>ЭкоМаркет</h1>
+          <div className={styles.ecomarket__filterButtons}>
+            <Button
+              type={"button"}
+              theme={"eco"}
+              onClick={onButtonClick}
+              children={"По популярности"}
+            />
 
-                        <Button
-                          type={"button"}
-                          theme={"eco"}
-                          onClick={onButtonClick}
-                          children={"По новизне"}
-                        />
-                    </div>
+            <Button
+              type={"button"}
+              theme={"eco"}
+              onClick={onButtonClick}
+              children={"По цене"}
+            />
+
+            <Button
+              type={"button"}
+              theme={"eco"}
+              onClick={onButtonClick}
+              children={"По новизне"}
+            />
+          </div>
+        </div>
+
+        <aside className={styles.filter}>
+          <form className={styles.filter__block}>
+
+            <h3>Пол</h3>
+
+            {genders.map((gender) => (
+              <CheckboxNew
+                isChecked={isCheck.includes(gender.id)}
+                id={gender.id}
+                text={gender.text}
+                handleClick={handleSelect}
+              />
+            ))}
+
+            <h3>Тип товара</h3>
+
+            <CheckboxNew
+              isChecked={isCheckedAll}
+              id={1}
+              text={"Выбрать все"}
+              handleClick={handleSelectAll}
+            />
+
+            {goodsType.map((goodsType) => (
+              <CheckboxNew
+                isChecked={isCheck.includes(goodsType.id)}
+                id={goodsType.id}
+                text={goodsType.text}
+                handleClick={handleSelect}
+              />
+            ))}
+
+            <h3>Брэнд</h3>
+
+            <CheckboxNew
+              isChecked={isCheckedAll}
+              id={1}
+              text={"Выбрать все"}
+              handleClick={handleSelectAll}
+            />
+
+            {brands.map((brand) => (
+              <CheckboxNew
+                isChecked={isCheck.includes(brand.id)}
+                id={brand.id}
+                text={brand.text}
+                handleClick={handleSelect}
+              />
+            ))}
+
+          </form>
+        </aside>
+
+        <section className={styles.productCards}>
+          <div className={styles.promocodeCard}>
+            <div className={styles.promocodeCard__info}>
+              <div className={styles.promocodeCard__info__text}>
+                <div className={styles.promocodeCard__info__balance}>
+                  <p>На вашем балансе</p>
+                  <img src={currency} alt="currency" />
+                  <h6>200</h6>
                 </div>
-
-                <aside className={styles.filter}>
-                    <form className={styles.filter__block}>
-
-                        <h3>Пол</h3>
-
-                        {genders.map((gender) => (
-                          <CheckboxNew text={gender.text}/>
-                        ))}
-
-                        {/*{genders.map((gender, index)=>(*/}
-                        {/*    <Checkbox*/}
-                        {/*      id={index}*/}
-                        {/*      text={gender.text}*/}
-                        {/*    />*/}
-                        {/*))}*/}
-
-                        <h3>Тип товара</h3>
-
-                        {goodsType.map((goodsType) => (
-                            <CheckboxNew text={goodsType.text}/>
-                        ))}
-
-                        {/*{goodsType.map((goodType, index)=>(*/}
-                        {/*  <Checkbox*/}
-                        {/*    id={index}*/}
-                        {/*    text={goodType.text}*/}
-                        {/*  />*/}
-                        {/*))}*/}
-
-                        <h3>Брэнд</h3>
-
-                        {brands.map((brand)=>(
-                          <CheckboxNew text={brand.text}/>
-                        ))}
-
-                        {/*{brands.map((brand, index)=>(*/}
-                        {/*  <Checkbox*/}
-                        {/*    id={index}*/}
-                        {/*    text={brand.text}*/}
-                        {/*  />*/}
-                        {/*))}*/}
-                    </form>
-                </aside>
-
-                <section className={styles.productCards}>
-                    <div className={styles.promocodeCard}>
-                        <div className={styles.promocodeCard__info}>
-                            <div className={styles.promocodeCard__info__text}>
-                                <div className={styles.promocodeCard__info__balance}>
-                                    <p>На вашем балансе</p>
-                                    <img src={currency} alt="currency"/>
-                                    <h6>200</h6>
-                                </div>
-                                <p>Вы можете обменять их на скидку 200 руб.</p>
-                            </div>
-                            <Button
-                                type="submit"
-                                onClick={onGetPromocodeClick}
-                                theme={"green"}
-                                children={"Получить промокод"}
-                            />
-                        </div>
-                    </div>
-                    <EcoMarketCard
-                        brand={"NIKE"}
-                        img={sneak1}
-                        name={"Nike Air Max 2021"}
-                        gender={"Мужская обувь"}
-                        price={"1000"}
-                        onClick={onGetQRGet}
-                    />
-                    <EcoMarketCard
-                        brand={"NIKE"}
-                        img={sneak2}
-                        name={"Nike Air Max 90 Premium"}
-                        gender={"Мужская обувь"}
-                        price={"750"}
-                        onClick={onGetQRGet}
-                    />
-                    <EcoMarketCard
-                        brand={"Adidas"}
-                        img={sneak3}
-                        name={"Adidas Alphabounce RC"}
-                        gender={"Мужская обувь"}
-                        price={"1200"}
-                        onClick={onGetQRGet}
-                    />
-                    <EcoMarketCard
-                        brand={"H&M"}
-                        img={hoodie}
-                        name={"Nike Air Max 2021"}
-                        gender={"Мужская обувь"}
-                        price={"1000"}
-                        onClick={onGetQRGet}
-                    />
-                    <EcoMarketCard
-                        brand={"NIKE"}
-                        img={sneak4}
-                        name={"Nike Air Force 1 Low"}
-                        gender={"Мужская обувь"}
-                        price={"2100"}
-                        onClick={onGetQRGet}
-                    />
-                </section>
-            </BaseLayout>
-        </>
-    )
-})
+                <p>Вы можете обменять их на скидку 200 руб.</p>
+              </div>
+              <Button
+                type="submit"
+                onClick={onGetPromocodeClick}
+                theme={"green"}
+                children={"Получить промокод"}
+              />
+            </div>
+          </div>
+          <EcoMarketCard
+            brand={"NIKE"}
+            img={sneak1}
+            name={"Nike Air Max 2021"}
+            gender={"Мужская обувь"}
+            price={"1000"}
+            onClick={onGetQRGet}
+          />
+          <EcoMarketCard
+            brand={"NIKE"}
+            img={sneak2}
+            name={"Nike Air Max 90 Premium"}
+            gender={"Мужская обувь"}
+            price={"750"}
+            onClick={onGetQRGet}
+          />
+          <EcoMarketCard
+            brand={"Adidas"}
+            img={sneak3}
+            name={"Adidas Alphabounce RC"}
+            gender={"Мужская обувь"}
+            price={"1200"}
+            onClick={onGetQRGet}
+          />
+          <EcoMarketCard
+            brand={"H&M"}
+            img={hoodie}
+            name={"Nike Air Max 2021"}
+            gender={"Мужская обувь"}
+            price={"1000"}
+            onClick={onGetQRGet}
+          />
+          <EcoMarketCard
+            brand={"NIKE"}
+            img={sneak4}
+            name={"Nike Air Force 1 Low"}
+            gender={"Мужская обувь"}
+            price={"2100"}
+            onClick={onGetQRGet}
+          />
+        </section>
+      </BaseLayout>
+    </>
+  );
+};
