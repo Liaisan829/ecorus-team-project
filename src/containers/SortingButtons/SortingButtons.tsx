6e20fcf0607303654329eb6688ff499683e40c37
Liaisan Akhmetova <akhmetova1423@gmail.com>
2022-04-25 12:35:32 +0300

    done

--- src/containers/SortingButtons/SortingButtons.tsx
+++ src/containers/SortingButtons/SortingButtons.tsx
@@ -1,47 +1,26 @@
-import axios from "axios";
-import {Button} from "../../components/ui/Button/Button";
+import { Button } from "../../components/ui/Button/Button";
 
 export const SortingButtons = () => {
 
-    const getItems = () => {
-        axios.get("market", {
-            params: {
-                page_number: 1,
-                page_size: 10,
-                sexes: `["MAN"]`,
-                item_categories: `["SHOES"]`,
-                shop_ids: `["ec4e2c1d-a495-4010-bbf9-67fc99c8be28"]`
-            }
-        })
-            .then()
-            .catch();
-    };
+  return (
+    <>
+      <Button
+        type={"button"}
+        theme={"eco"}
+        children={"По популярности"}
+      />
 
-    const onButtonClick = () => {
-        getItems();
-    };
-    return (
-        <>
-            <Button
-                type={"button"}
-                theme={"eco"}
-                onClick={onButtonClick}
-                children={"По популярности"}
-            />
+      <Button
+        type={"button"}
+        theme={"eco"}
+        children={"По цене"}
+      />
 
-            <Button
-                type={"button"}
-                theme={"eco"}
-                onClick={onButtonClick}
-                children={"По цене"}
-            />
-
-            <Button
-                type={"button"}
-                theme={"eco"}
-                onClick={onButtonClick}
-                children={"По новизне"}
-            />
-        </>
-    );
+      <Button
+        type={"button"}
+        theme={"eco"}
+        children={"По новизне"}
+      />
+    </>
+  );
 };
