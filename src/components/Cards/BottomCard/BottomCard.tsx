cdac261d8e00745bdfe0b425ca119d2cafecb171
Liaisan Akhmetova <akhmetova1423@gmail.com>
2022-04-26 12:39:31 +0300

    done

--- src/components/Cards/BottomCard/BottomCard.tsx
+++ src/components/Cards/BottomCard/BottomCard.tsx
@@ -1,45 +1,43 @@
-import {FC} from "react";
-import {useNavigate} from "react-router";
-import {Button} from "../../ui/Button/Button";
-import {Icon} from "../../ui/Icon/Icon";
+import { FC } from "react";
+import { useNavigate } from "react-router";
+import { Button } from "../../ui/Button/Button";
+import { Icon } from "../../ui/Icon/Icon";
 import styles from "./BottomCard.module.scss";
 
 interface BottomCardProps {
-    cardHeadline: string,
-    cardText: string,
-    img: any,
-    path: string
+  cardHeadline: string,
+  cardText: string,
+  img: any,
+  path: string
 }
 
-export const BottomCard: FC<BottomCardProps> = ({cardHeadline, cardText, img, path}) => {
+export const BottomCard: FC<BottomCardProps> = ({ cardHeadline, cardText, img, path }) => {
 
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const onBottomButtonClick = () => {
-        navigate(path);
-    };
+  const onBottomButtonClick = () => {
+    navigate(path);
+  };
 
-    return (
-        <div className={styles.informBlock}>
-            <>
-                <div className={styles.informBlock__info}>
-                    <div className={styles.informBlock__info__text}>
-                        <h1>{cardHeadline}</h1>
-                        <p>{cardText}</p>
-                    </div>
-                    <div className={styles.informBlock__info__cardButton}>
-                        <Button
-                            type="button"
-                            onClick={onBottomButtonClick}
-                            theme="grey"
-                            disabled={false}
-                        >
-                            <Icon name="buttonArrow" width="24" height="24"/>
-                        </Button>
-                    </div>
-                </div>
-                <img src={img} alt="image"/>
-            </>
+  return (
+    <div className={styles.informBlock}>
+      <div className={styles.informBlock__info}>
+        <div className={styles.informBlock__info__text}>
+          <h1>{cardHeadline}</h1>
+          <p>{cardText}</p>
         </div>
-    );
+        <div className={styles.informBlock__info__cardButton}>
+          <Button
+            type="button"
+            onClick={onBottomButtonClick}
+            theme="grey"
+            disabled={false}
+          >
+            <Icon name="buttonArrow" width="24" height="24" />
+          </Button>
+        </div>
+      </div>
+      <img src={img} alt="image" />
+    </div>
+  );
 };
